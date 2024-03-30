59f6071fd04ed84495ebc849538ba144a70579a0
dipaksuralkar <dipaksuralkar7249@gmail.com>
2024-03-30 08:55:12 +0530

    completed but tkinter remaining

--- rand_pass.py
+++ rand_pass.py
@@ -1,25 +1,33 @@
 import random
 import string
-import tkinter as tk
+import tkinter
 
-#root = tk.Tk()
-#root.geometry("400x500")
+root = tk.Tk()
 
-num = [0,1,2,3,4,5,6,7,8,9]
-symbals = ["!","@","#","$","%","^","&","*"]
-upper_str = list(string.ascii_uppercase)
-lower_str = list(string.ascii_lowercase)
-combine = num + symbals + upper_str + lower_str
-n = random.choices(combine, k=23)
+def generate_random_password(length):
+        # Define character sets
+    num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    symbols = ["!", "@", "#", "$", "%", "^", "&", "*"]
+    upper_str = list(string.ascii_uppercase)
+    lower_str = list(string.ascii_lowercase)
+
+        # Convert num to strings
+    num = [str(x) for x in num]
+
+        # Combine character sets
+    combine = num + symbols + upper_str + lower_str
+
+        # Generate random password
+    password = ''.join(random.choices(combine, k=length))
+    return password
+    # Prompt user for password length
+length = int(input("Enter the length of the password: "))
+
+    # Generate random password
+password = generate_random_password(length)
+
+    # Print the generated password
+print("Random Password:", password)
 
 
-#combine = str(num) + str(symbals) + str(upper_str) + str(lower_str)
-print("**** Random Password Generator ****")
-str = ""
-print(n)
 
-#n = int(input("Enter the Length of Password  :-"))
-#n = random.choices(combine, k = n)
-#print("".join(n))
-
-#root.mainloop()
